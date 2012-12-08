1f8ede093df973ab9c9351adb7dd0c3025529655
Dave Haenze <dah33@aber.ac.uk>
2012-12-08 17:41:49 +0000

    renamed package, updated the reference.

---
+++ groupProject/src/java/servlets/Register.java
@@ -0,0 +1,96 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import database.Person;
+import database.PersonDOA;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author thomas
+ */
+@WebServlet(name = "Register", urlPatterns = {"/register"})
+public class Register extends HttpServlet {
+    @EJB PersonDOA personDOA;
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+         String temp_name = request.getParameter("first_name") + " " + request.getParameter("last_name");
+         String temp_email = request.getParameter("email");
+         String temp_password = request.getParameter("password");
+         String formError =" ";
+         
+         if(personDOA.lookForEmail(temp_email)){
+             formError ="Email address is already in the system";
+             request.setAttribute("error", formError);
+             request.getRequestDispatcher("/register.jsp").forward(request, response);
+         }
+         else{
+             this.personDOA.persist(new Person(temp_name, temp_password, temp_email));
+         
+             response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+             response.setHeader("Location", "http://localhost:8080/MonsterGame/login.jsp");
+         }   
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
