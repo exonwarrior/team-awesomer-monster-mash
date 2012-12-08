21f860ba4995fdbd2cf24be186cb96dd0691b853
Dave Haenze <dah33@aber.ac.uk>
2012-12-08 17:05:06 +0000

    Added class file

---
+++ groupProject/src/java/database/MonsterDOA.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Dave
+ */
+@Stateless
+public class MonsterDOA {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+}
