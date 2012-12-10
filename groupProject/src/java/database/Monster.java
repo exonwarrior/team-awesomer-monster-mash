dc215f43ff1f6ae1f8c74ae3366c185215f7f847
Dave Haenze <dah33@aber.ac.uk>
2012-12-10 08:23:57 +0000

    Updates

--- groupProject/src/java/database/Monster.java
+++ groupProject/src/java/database/Monster.java
@@ -109,4 +109,19 @@ public class Monster implements Serializable {
         return "database.Monster[ id=" + id + " ]";
     }
     
+    +    public Monster generateRandom(Person user){
+         Monster m = new Monster();
+         Random random = new Random();
+         m.setStrength(random.nextInt(11));
+         m.setGenes(random.nextInt(11));
+         m.setEvade(random.nextInt(11));
+         m.setHealth(random.nextInt(11));
+         m.setPrice(m.getStrength()+m.getEvade()+m.getHealth()+m.getGenes());
+         m.setName(JOptionPane().showInputDialog(null, "How would you like to name your monster?"));
+-        m.setOwner();
++        m.setOwner(user.getName());
+         return m;
+     }
+
+    
 }
